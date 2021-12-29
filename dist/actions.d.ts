3d582c95f0c5009cc36a5570a8c41ce17bd4ab62
Dong-Young Kim <31337.persona@gmail.com>
2021-12-30 02:07:04 +0900

    build: add dist directory to ignore list

--- dist/actions.d.ts
+++
@@ -1,6 +0,0 @@
-import { Command, OptionValues } from "commander";
-export declare const validateRepositoryArgument: (repository: string) => [string, string];
-export declare const printLabels: ([owner, repo]: [string, string], _: OptionValues, command: Command) => Promise<void>;
-export declare const exportLabels: ([owner, repo]: [string, string], options: OptionValues, command: Command) => Promise<void>;
-export declare const importLabels: ([owner, repo]: [string, string], options: OptionValues, command: Command) => Promise<void>;
-export declare const clearLabels: ([owner, repo]: [string, string], _: OptionValues, command: Command) => Promise<void>;
--- dist/types.d.ts
+++
@@ -1,7 +0,0 @@
-import { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
-export declare type CreateLabelParamers = RestEndpointMethodTypes["issues"]["createLabel"]["parameters"];
-export declare type UpdateLabelParamers = RestEndpointMethodTypes["issues"]["updateLabel"]["parameters"];
-export declare type LabelReponse = RestEndpointMethodTypes["issues"]["getLabel"]["response"]["data"];
-export declare type Label = Pick<LabelReponse, "name" | "color"> & {
-    description?: string;
-};
